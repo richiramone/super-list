9968eea5f9217704f6014af35e13abdad4d89bcf
Lucas Ramos <ciao@lucasramos.me>
2016-01-23 21:08:28 +0100

    1.1.0 released

---
+++ releases/1.1.0/api/actions/get.php
@@ -0,0 +1,4 @@
+<?php
+  $json = file_get_contents(JSON);
+  $items = json_decode($json, true); 
+?>
---
+++ releases/1.1.0/api/config.php
@@ -0,0 +1,5 @@
+<?php
+  define("JSON", DOCROOT . '/super-list.json');
+  define("MAIL_LUCAS", 'ramoslucasd@gmail.com');
+  define("MAIL_ANNA", 'dallarianna@gmail.com');
+?>
