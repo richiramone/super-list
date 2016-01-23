a57bd5e49da274a9cf3ed3509ece6e4668730f57
Lucas Ramos <ciao@lucasramos.me>
2016-01-23 21:05:53 +0100

    1.1.0-rc...

--- rc/api/actions/delete.php
+++ rc/api/actions/delete.php
@@ -2,10 +2,14 @@
   $json = file_get_contents(JSON);
   $items = json_decode($json, true);
 
-  unset($items["items"][$_GET["id"]]);
+  if ($_GET["empty"] == true) {
+    $items["items"] = array();
+  } else {
+    unset($items["items"][$_GET["id"]]);
+  }
 
   $items['items'] = array_values($items['items']);
   $newJson = json_encode($items);
-  
+
   file_put_contents(JSON, $newJson);
 ?>
