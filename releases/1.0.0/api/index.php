fe114416f1ba5b1312729cd2c8817daace2ef629
Lucas Ramos <ciao@lucasramos.me>
2016-08-19 20:17:43 +0200

    ...

--- releases/1.0.0/api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  define("DOCROOT", $_SERVER["DOCUMENT_ROOT"] . '/api');
-
-  include_once(DOCROOT . '/auth.php');
-  include_once(DOCROOT . '/config.php');
-  include_once(DOCROOT . '/security.php');
-  include_once(DOCROOT . '/router.php');
-?>
--- releases/1.1.1/api/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-  define("JSON", DOCROOT . '/super-list.json');
-  define("MAIL_LUCAS", 'ramoslucasd@gmail.com');
-  define("MAIL_ANNA", 'dallarianna@gmail.com');
-?>
