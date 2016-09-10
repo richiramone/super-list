3647d95611776115c2617471f1f0f63780bb38a9
Lucas Ramos <ciao@lucasramos.me>
2016-09-10 19:36:17 +0200

    dragAndDropTouch support added

--- dev/index.php
+++ dev/index.php
@@ -88,6 +88,7 @@
   </section>
 
   <script src="js/vendor/jquery-2.1.4.min.js"></script>
+  <script src="js/vendor/dragAndDropTouch.min.js"></script>
   <script src="js/app.js" async></script>
 </body>
 </html>
