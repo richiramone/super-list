5e38b14a5583b9ddeeb4d4b27b38e4b2f3b2b256
Lucas Ramos <ciao@lucasramos.me>
2016-09-10 19:06:01 +0200

    releasing 1.2.0

---
+++ rc/api/views/get.php
@@ -0,0 +1,38 @@
+<?php
+  $output = "";
+  $i = 0;
+
+  foreach ($items["items"] as $item) {
+    $output .= appendContent($item, $i);
+    $i++;
+  }
+
+  $output .= appendContent(null, $i);
+
+  function appendContent($item, $i) {
+    $content = $item["content"] ? $item["content"] : "";
+    $itemStatusClass = $item == null ? 'new' : 'existing';
+
+    $html = "";
+    $template = '
+    <div class="{{ITEM_STATUS}} item" data-item="{{ID}}" data-item-status="{{ITEM_STATUS}}" draggable="true">
+      <input id="item-{{ID}}" type="hidden">
+      <label for="item-{{ID}}"></label>
+      <h2 data-trigger-item-content>{{CONTENT}}</h2>
+      <input type="text" value="{{CONTENT}}">
+      <button data-trigger-delete>
+        <svg viewBox="0 0 32 32">
+          <use xlink:href="#shape-trash"></use>
+        </svg>
+      </button>
+    </div>
+    ';
+
+    $html .= $template;
+    $html = str_replace('{{ITEM_STATUS}}', $itemStatusClass, $html);
+    $html = str_replace('{{ID}}', $i, $html);
+    $html = str_replace('{{CONTENT}}', $content, $html);
+
+    return $html;
+  }
+?>
