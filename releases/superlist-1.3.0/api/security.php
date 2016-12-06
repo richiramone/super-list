743c6a435a2f0bd5f9b709edc5aa2835c5429900
Lucas Ramos <ciao@lucasramos.me>
2016-12-06 15:48:53 +0100

    ...

---
+++ releases/superlist-1.3.0/api/security.php
@@ -0,0 +1,7 @@
+<?php
+  if ($_GET['action'] == 'put' || $_GET['action'] == 'update') {
+    foreach($_GET as $key => $value) {
+      $_GET[$key] = htmlspecialchars($value, ENT_QUOTES|'ENT_SUBSTITUE', "UTF-8");
+    }
+  }
+?>
