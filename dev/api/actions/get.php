ab1ec50ef56c3f70ed9ca49d12ed7073ddd0b2d2
Lucas Ramos <ciao@lucasramos.me>
2017-01-31 13:21:14 +0100

    api sort bug fix

--- dev/api/actions/get.php
+++ dev/api/actions/get.php
@@ -1,4 +1,4 @@
 <?php
   $json = file_get_contents(JSON);
-  $items = json_decode($json, true); 
+  $items = json_decode($json, true);
 ?>
--- dev/api/actions/put.php
+++ dev/api/actions/put.php
@@ -2,7 +2,7 @@
   $json = file_get_contents(JSON);
   $items = json_decode($json, true);
 
-  array_push($items["items"], array("content" => $_GET["content"]));
+  array_unshift($items["items"], array("content" => $_GET["content"]));
 
   $items['items'] = array_values($items['items']);
   $newJson = json_encode($items);
