0489fd6d533b1f568a2c2f2073b009fc91328a23
Lucas Ramos <lucas_r@me.com>
2022-04-06 17:45:52 +0200

    fix renbame

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
-import Preloader from "./components/Preloader/Preloader";
-import Header from "./components/Header/Header";
-import AddItem from "./components/AddItem";
-import ItemsList from "./components/ItemList/ItemsList";
-import styled from "styled-components";
+import './App.css';
+import Preloader from './Components/Preloader';
+import Header from './Components/Header';
+import AddItem from './Components/AddItem';
+import ItemsList from './Components/ItemList';
+import styled from 'styled-components';
 
 const MainStyles = styled.main`
   margin-top: 60px;
