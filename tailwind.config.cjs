9b703eb3962aded0688e438d9aaa62fc5f140685
Lucas Ramos <richiramone@users.noreply.github.com>
2022-12-01 10:18:21 +0100

    ..

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      spacing: {
-        'header-heigth': '124px',
-      },
       colors: {
         primary: '#009dff',
         'item-is-duplicated': '#223889',
