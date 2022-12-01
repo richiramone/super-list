f2313ffe97f9f1672a4786ee389934701a860d67
Lucas Ramos <richiramone@users.noreply.github.com>
2022-12-01 10:12:18 +0100

    header height

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      spacing: {
+        'header-heigth': '124px',
+      },
       colors: {
         primary: '#009dff',
         'item-is-duplicated': '#223889',
