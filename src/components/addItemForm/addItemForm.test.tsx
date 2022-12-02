a2c023a41262414921721bd9cb10e9a71f9e2629
Lucas Ramos <richiramone@users.noreply.github.com>
2022-12-02 12:12:44 +0100

    it fkn works

--- src/components/addItemForm/addItemForm.test.tsx
+++ src/components/addItemForm/addItemForm.test.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
 import { it } from 'vitest';
+import AddItemForm from './addItemForm';
 
 it('should work', ctx => {
+  renderer.create(<AddItemForm />);
   // prints name of the test
   console.log(ctx.meta.name);
+  expect(true).toBeTruthy();
 });
