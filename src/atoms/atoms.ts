564e6acca5f743c6fa7551262d70a0190678b32b
Lucas Ramos <richiramone@users.noreply.github.com>
2022-12-07 17:34:23 +0100

    author bugfix

--- src/atoms/atoms.ts
+++ src/atoms/atoms.ts
@@ -3,4 +3,4 @@ import { atom } from 'jotai';
 export const isOnlineAtom = atom<boolean>(true);
 export const isLoadingAtom = atom<boolean>(true);
 export const needsRefreshAtom = atom<number>(0);
-export const authorAtom = atom<string>('');
+export const authorAtom = atom<string | null>(localStorage.getItem('author'));
