4370d8fc99956d4c4d44d3229b18fc870a48c162
Lucas Ramos <richiramone@users.noreply.github.com>
2022-12-09 12:03:51 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ssr from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ssr()],
   test: {
     globals: true,
     environment: 'jsdom',
