a9fe97951571c2bc36834cc66d35b7474a76df9c
Lucas Ramos <richiramone@users.noreply.github.com>
2022-12-09 11:13:08 +0100

    rollback

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ssr from 'vite-plugin-ssr/plugin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ssr()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
